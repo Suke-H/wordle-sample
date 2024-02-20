8d275915a194f46d86d711f4d149e06c67dc339f
Suke-H <hondii1997@gmail.com>
2024-02-20 19:45:21 +0900

    refactor: pushedEnterProcessの引数を減らした

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,15 @@ import { makeGameResultText } from "./utils/makeGameResultText";
 
 export const App = (): JSX.Element => {
 
-  // 回答欄の文字列
+  // （リスト初期化）
   const initAnswerList: string[][] = new Array(6);
   for (let i = 0; i < 6; i++) {
     initAnswerList[i] = new Array(5).fill("");
   }
+  /* 回答欄の文字列 */
   const [answerList, setAnswerList] = useState<string[][]>(initAnswerList);
 
-  // 回答欄のマッチ状況
+  /* 回答欄のマッチ状況 */
   // White: 判定していない
   // Black: 文字も位置も無一致
   // Yellow: 文字のみ一致
@@ -29,39 +30,55 @@ export const App = (): JSX.Element => {
   }
   const [ matchList, setMatchList ] = useState<string[][]>(initMatchList);
 
-  // 回答の判定を行うフラグ
+  /* 回答の判定を行うフラグ */
   // キーボードのEnter入力により更新
   const [judge, setJudge] = useState<boolean>(false);
 
-  // 正解単語
+  /* 正解単語 */
   const [correctAnswer, setCorrectAnswer] = useState<string>("");
-  // 今日が何回目のゲームか
+  /* 今日が何回目のゲームか */
   const [todaysNo, setTodaysNo] = useState<number>(0);
 
-  // ラウンド（=現在の行番号+1）
+  /* ラウンド（=現在の行番号+1）*/
   const [round, setRound] = useState<number>(0);
   const [columncnt, setColumncnt] = useState(0);
 
-  // 初回レンダリング時にのみ実行
+  // 初回レンダリング時
    useEffect(() => {
+    // 本日の単語を取得
     getTodaysWord(setCorrectAnswer, setTodaysNo);
+    // ラウンドを1に設定
     setRound(round + 1);
   }, []);
 
   // Enterを押した際
   useEffect(() => {
-    pushedEnterProcess(
-      judge, 
-      setJudge,
-      correctAnswer,
-      answerList,
-      setAnswerList,
-      matchList,
-      round,
-      setRound,
-      setColumncnt,
-      setMatchList
-    );
+      if (!judge) return;
+
+      pushedEnterProcess(correctAnswer, answerList, matchList,round,setMatchList)
+          .then((isValid) => {
+                /* 単語が妥当でない場合 */
+                if (!isValid) {
+                alert("データセットに存在しない単語です");
+                // 回答欄を1行リセット
+                setAnswerList((prevState) =>
+                  prevState.map((row, index) =>
+                    index === round - 1 ? Array(5).fill("") : row
+                  )
+                );
+                setColumncnt(0);
+                } 
+
+                /* 問題ない場合 */
+                else {
+                  // 次の行へ移行
+                  setRound(round + 1);
+                  setColumncnt(0);
+                }
+            });
+
+      setJudge(false);
+
   }, [judge]);
 
   return (
--- src/game_logics/pushedEnterProcess.ts
+++ src/game_logics/pushedEnterProcess.ts
@@ -3,45 +3,24 @@ import { checkWordMatch } from "./checkWordMatch";
 import { checkClear } from "./checkClear";
 
 export const pushedEnterProcess = async ( 
-      judge: boolean, 
-      setJudge: React.Dispatch<React.SetStateAction<boolean>>,
       correctAnswer: string,
       answerList: string[][],
-      setAnswerList: React.Dispatch<React.SetStateAction<string[][]>>,
       matchList: string[][],
       round: number,
-      setRound: React.Dispatch<React.SetStateAction<number>>,
-      setColumncnt: React.Dispatch<React.SetStateAction<number>>,
       setMatchList: React.Dispatch<React.SetStateAction<string[][]>>,
-  ) => {
+  ) : Promise<boolean> => {
 
-    // Enterを押したら
-    if (judge === true) {
-      // 単語の妥当性判定
-      const isValid = await checkWordValidity(answerList, round);
-      if (!isValid)
-      {
-        alert("データセットに存在しない単語です");
-        setAnswerList(prevState =>
-          prevState.map((row, index) =>
-            index === round-1 ? Array(5).fill("") : row
-          )
-        );
-        setColumncnt(0);
-        return;
-      }
+    // 単語の妥当性判定
+    const isValid = await checkWordValidity(answerList, round);
+    if (!isValid) return false;
 
-        // 単語一致判定
-        const tmpMatchList = checkWordMatch(correctAnswer, answerList, matchList, round);
-        // クリア判定
-        checkClear(correctAnswer, answerList, round);
-        // スタイル更新
-        setMatchList(tmpMatchList);
-        // ラウンド更新
-        setColumncnt(0);
-        setRound(round + 1);
-
-        // フラグを戻す
-        setJudge(false);
-    }
+    // 単語一致判定
+    const tmpMatchList = checkWordMatch(correctAnswer, answerList, matchList, round);
+    // クリア判定
+    checkClear(correctAnswer, answerList, round);
+    
+    // スタイル更新
+    setMatchList(tmpMatchList);
+  
+    return true;
   }
